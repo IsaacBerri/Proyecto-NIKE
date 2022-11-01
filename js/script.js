a87418a1b83dabb038a6b86f15e888a214cdb4d3
Isaac Berrio <berriisaax18@hotmail.com>
2022-11-01 16:59:02 -0500

    Empezando con JavaScript

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let zapatosJordan = [
+    {id: "Jordan-1-Red", nombre: "Jordan 1 Retro Chicago", precio: 160},
+    {id: "Jordan-1-Blue", nombre: "Jordan 1 University Blue", precio: 395},
+    {id: "Jordan-1-Black", nombre: "Jordan 1 Black White", precio: 600},
+    {id: "Jordan-1-Dior", nombre: "Jordan 1 Retro High Dior", precio: 7700},
+    {id: "Jordan-4-Black", nombre: "Jordan 4 black canvas", precio: 302},
+    {id: "Jordan-4-Blue", nombre: "Jordan 4 University Blue", precio: 436},
+    {id: "Jordan-4-White", nombre: "Jordan 4 White Oreo", precio: 445},
+    {id: "Jordan-4-Shimmer", nombre: "Jordan 4 Shimmer", precio: 380},
+    {id: "Jordan-6-Green", nombre: "Jordan 6 Electric Green", precio: 200},
+    {id: "Jordan-6-Grey", nombre: "Jordan 6 Georgetown", precio: 171},
+    {id: "Jordan-6-Black", nombre: "Jordan 6 Infrared Black", precio: 239},
+    {id: "Jordan-6-Travis-Scott", nombre: "Jordan 6 Travis Scott", precio: 390}
+]
